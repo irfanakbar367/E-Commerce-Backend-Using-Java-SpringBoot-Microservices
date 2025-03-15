86c150a20d5ce5f4cf9a4917a9eef71b94cf4136
Irfan <mianirfan367@gmail.com>
2025-03-15 20:46:41 +0500

    login tests written

--- UserService/src/test/java/com/example/UserService/services/UserServiceTest.java
+++ UserService/src/test/java/com/example/UserService/services/UserServiceTest.java
@@ -9,16 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,12 +25,17 @@ class UserServiceTest {
     @Mock
     private UserRepo userRepo;
 
+    @Mock
     private AuthenticationManager authenticationManager;
     @Mock
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     private Users user;
     @InjectMocks
     private UserService userService;
+    @Mock
+    private Authentication authentication;
+    @Mock
+    private JwtService jwtService;
 
     @BeforeEach
     void setUp() {
@@ -56,19 +59,66 @@ class UserServiceTest {
     }
 
     @Test
-    void addUser() {
+    void shouldSaveUserSuccessfully() {
         when(bCryptPasswordEncoder.encode(anyString())).thenReturn("hashedPassword");
-        when(userRepo.save(user)).thenReturn(user);
+        when(userRepo.save(any(Users.class))).thenReturn(user);
 
         String result = userService.addUser(user);
 
         assertEquals("User Saved", result);
         assertEquals("hashedPassword", user.getPassword());
-
         verify(userRepo, times(1)).save(user);
     }
 
     @Test
-    void login() {
+    void shouldReturnFailureMessageOnException() {
+        doThrow(new RuntimeException("Database error")).when(userRepo).save(any(Users.class));
+
+        String result = userService.addUser(user);
+
+        assertEquals("User save failed ", result);
+    }
+
+    @Test
+    void shouldReturnFailureMessageOnEncryptionException() {
+        when(bCryptPasswordEncoder.encode(anyString())).thenThrow(new RuntimeException("Not Encrypted"));
+
+        String result = userService.addUser(user);
+
+        assertEquals("User save failed ", result);
+    }
+
+
+    @Test
+    void loginShouldReturnTokenIfAuthenticated() {
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).
+                thenReturn(authentication);
+        when(authentication.isAuthenticated()).thenReturn(true);
+        when(jwtService.generateToken(anyString())).thenReturn("mocked-jwt-token");
+
+        String result = userService.login(user);
+
+        verify(authenticationManager, times(1))
+                .authenticate(new UsernamePasswordAuthenticationToken
+                        (user.getUsername(), user.getPassword()));
+        verify(authentication, times(1)).isAuthenticated();
+        verify(jwtService, times(1)).generateToken(user.getUsername());
+        assertEquals(result, "mocked-jwt-token");
+    }
+
+    @Test
+    void loginShouldReturnExceptionIfNotAuthenticated() {
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).
+                thenReturn(authentication);
+        when(authentication.isAuthenticated()).thenReturn(false);
+
+        String result = userService.login(user);
+
+        verify(authenticationManager, times(1))
+                .authenticate(new UsernamePasswordAuthenticationToken
+                        (user.getUsername(), user.getPassword()));
+        verify(authentication, times(1)).isAuthenticated();
+        verify(jwtService, times(0)).generateToken(user.getUsername());
+        assertEquals(result, "User Not Found.");
     }
 }
